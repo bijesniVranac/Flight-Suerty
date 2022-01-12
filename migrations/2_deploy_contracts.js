b9121d43a1f122a9616693f7933601f22db28988
Djordje Dajkovic <djordje.dajkovic@gmail.com>
2022-01-12 21:35:06 +0100

    Add files via upload

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function (deployer) {
+
+	let firstAirline = '0x49856BBf9bd3864BAC5c99602Cd35DD88dE654eB';
+	deployer.deploy(FlightSuretyData)
+		.then(() => {
+			return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+				.then(() => {
+					let config = {
+						localhost: {
+							url: 'http://localhost:8545',
+							dataAddress: FlightSuretyData.address,
+							appAddress: FlightSuretyApp.address
+						}
+					}
+					fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+					fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+				});
+		});
+}
---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 8545,
+			network_id: '*',
+			gas: 9999999
+		}
+	},
+	compilers: {
+		solc: {
+			version: "^0.4.24"
+		}
+	}
+};
